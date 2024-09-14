c5601bc97b23cdffa7c559b57dd4796c7c4ed815
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-09-14 22:12:30 +0300

    added the html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App 🌬</h1>
-
+        <Weather />
         <footer>
           This project was coded by Haya Tamimi ♥ and is open-source on{" "}
           <a
