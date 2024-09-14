7d84640b98b64105f8c392d50e6d4c503feb804b
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-09-14 21:09:28 +0300

    added the git repo link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-  Weather App 🌬
+      <h1>Weather App 🌬</h1>
+      <footer>
+        This project was coded by Haya Tamimi ♥ and is open-source on{" "}
+        <a href="https://github.com/HayaTamimi/react-weather" target="_blank">
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
