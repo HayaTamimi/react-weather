1fd65ff45a8ff332be7255006b43530b3da4baaf
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-09-14 21:28:46 +0300

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App 🌬</h1>
-      <footer>
-        This project was coded by Haya Tamimi ♥ and is open-source on{" "}
-        <a href="https://github.com/HayaTamimi/react-weather" target="_blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App 🌬</h1>
+
+        <footer>
+          This project was coded by Haya Tamimi ♥ and is open-source on{" "}
+          <a href="https://github.com/HayaTamimi/react-weather" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
